dee91218d266f09b06414fe4be7ea301eff39b6b
blyat <daniel.bermude@gmail.com>
2016-02-01 21:06:15 -0600

    Lab 4 SQL Database
    Database de Direcciones

---
+++ Lab4Database/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Direcciones]
+(
+	[Nombre] NVARCHAR(50) NOT NULL , 
+    [Apellidos] NVARCHAR(50) NOT NULL, 
+    [Ciudad] NVARCHAR(50) NOT NULL, 
+    [Direccion] NVARCHAR(50) NOT NULL, 
+    [Telefono] NVARCHAR(50) NOT NULL, 
+    PRIMARY KEY ([Nombre], [Apellidos])
+)
